864ddbed44fcaf0b65c8df2d9994399ac87d7678
roney ruiz <roneyruizrojas@gmail.com>
2025-02-18 09:43:11 -0600

    feat: implement pinia

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import "./style.css";
 import App from "./App.vue";
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 
 import { Theme } from "./theme";
 import PrimeVue from "primevue/config";
 import { Router } from "@core/routes/router";
 
 const app = createApp(App);
+const Pinia = createPinia();
 
+app.use(Pinia);
 app.use(Router);
 app.use(PrimeVue, { theme: Theme });
 
