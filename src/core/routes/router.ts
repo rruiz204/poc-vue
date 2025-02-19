3074429a395515a2da636f20f697cfbb6e418863
roney ruiz <roneyruizrojas@gmail.com>
2025-02-18 23:11:59 -0600

    feat: modify routes

--- src/core/routes/router.ts
+++ src/core/routes/router.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "@modules/home/HomePage.vue";
+import ProjectPage from "@modules/project/ProjectPage.vue";
 
 export const Router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", component: HomePage },
+    { path: "/", redirect: "/project" },
+    { path: "/project", component: ProjectPage },
   ],
 });
