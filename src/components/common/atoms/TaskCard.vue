6a380d4770a533c2d8c6dce552335453293ed560
roney ruiz <roneyruizrojas@gmail.com>
2025-02-18 23:11:29 -0600

    feat: build task card and tasks dialog

---
+++ src/components/common/atoms/TaskCard.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { Card } from 'primevue';
+import type { Task } from '@core/models/Task';
+import { FormatHelper } from "@core/helpers/FormatHelper";
+
+defineProps<{
+  task: Task;
+}>();
+</script>
+
+<template>
+  <Card class="border-2">
+    <template #subtitle>
+      <div class="flex justify-between mb-2">
+        <p>{{ task.name }}</p>
+        <p class="hidden md:block">Prioridad: {{ task.priority }}</p>
+      </div>
+
+      <div class="flex justify-between">
+        <p>Finaliza: {{ FormatHelper.formatDate(task.due_date) }}</p>
+        <p>Status: {{ task.status }}</p>
+      </div>
+    </template>
+  </Card>
+</template>
---
+++ src/components/common/molecules/TasksDialog.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { Dialog } from 'primevue';
+import type { Task } from '@core/models/Task';
+import TaskCard from '@components/common/atoms/TaskCard.vue';
+
+defineProps<{
+  tasks: Task[];
+  visible: boolean;
+}>();
+
+const emit = defineEmits(["update:visible"]);
+</script>
+
+<template>
+  <Dialog :visible="visible" @update:visible="emit('update:visible', $event)" modal header="Tareas del proyecto" class="w-[800px]">
+    <div class="flex flex-col gap-3">
+      <TaskCard v-if="tasks.length > 0" v-for="task in tasks" :key="task.id" :task="task" />
+      <div v-else class="p-4">
+        <p class="font-semibold text-center">Este proyecto no tiene tareas aún</p>
+      </div>
+    </div>
+  </Dialog>
+</template>
